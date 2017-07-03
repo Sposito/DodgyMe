40b74f535e9019f0c2698abcc172c1ea581e5228
Sposito <thiago.sposito@gmail.com>
2017-07-03 00:47:31 -0300

    Tree Adition

---
+++ Assets/TreeBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class TreeBehaviour : MonoBehaviour
+{
+
+    public Vector3 trunkSize = Vector3.one;
+    public Vector3 leavesSize = Vector3.one;
+
+    GameObject trunk;
+    GameObject trunkTip;
+    GameObject leavesPivot;
+    GameObject leaves;
+
+    void Awake()
+    {
+        trunk = transform.Find("TreeTrunk").gameObject;
+        trunkTip = transform.Find("TreeTrunkTip").gameObject;
+        leavesPivot = trunkTip.transform.Find("LeavesPivot").gameObject;
+        leaves = leavesPivot.transform.Find("Leaves").gameObject;
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+#if UNITY_EDITOR
+
+        ApplySizes();
+
+        #endif
+	}
+
+   public void ApplySizes(){
+        transform.localScale = trunkSize;
+        trunkTip.transform.localScale = new Vector3(
+            1f / transform.localScale.x * leavesSize.x,
+            1f / transform.localScale.y * leavesSize.y,
+            1f / transform.localScale.z * leavesSize.z);
+    }
+}
---
+++ Assets/TreeRandomizer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeRandomizer : MonoBehaviour {
+
+    TreeBehaviour treeBehaviour;
+    public Vector2 leavesVar = Vector2.zero;
+    public Vector2 trunkVar = Vector2.zero;
+	void Start () {
+        treeBehaviour = GetComponent<TreeBehaviour>();
+        treeBehaviour.trunkSize += Vector3.up * Random.Range(trunkVar.x, trunkVar.y);
+        treeBehaviour.leavesSize += Vector3.up * Random.Range(leavesVar.x, leavesVar.y);
+        treeBehaviour.ApplySizes();
+
+        Destroy(treeBehaviour, 0f);
+        Destroy(this, 0f);
+	}
+	
+	
+}
