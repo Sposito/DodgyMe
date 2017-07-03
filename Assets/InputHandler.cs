10fc304f12af3cbed9a629fa6368d2335be5ab81
Sposito <thiago.sposito@gmail.com>
2017-07-03 00:48:52 -0300

    touch handler

---
+++ Assets/InputHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Prime31;
+
+public class InputHandler : MonoBehaviour {
+
+    TKLSwipeDetector detector;
+    public LevelController levelController;
+    public static InputHandler singleton;
+
+	void Awake () {
+        DontDestroyOnLoad(gameObject);
+        detector = GetComponent<TKLSwipeDetector>();
+        if (singleton == null){
+            singleton = this;
+        }
+
+        detector.onSwipeDeteced += HandleInput;
+	}
+
+    void HandleInput(SwipeDirection dir){
+        if (levelController != null){
+            levelController.HandleSwipe(dir);
+        }
+    }
+	
+
+}
