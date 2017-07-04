46ccbc17079de54c9fc5b875a0cb36d7294b1fdb
Sposito <thiago.sposito@gmail.com>
2017-07-04 15:53:35 -0300

    Scripts Organization

---
+++ Assets/Scripts/RotateAround.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour {
+
+	public bool x;
+	public bool y;
+	public bool z;
+	public float speed = 1f;
+	protected Vector3 axis;
+	void Start() {
+		axis = new Vector3(x ? 1 : 0, y ? 1 : 0, z ? 1 : 0);
+	}
+
+	// Update is called once per frame
+	void Update() {
+		RotateIt();
+	}
+
+	public virtual void RotateIt() {
+		transform.Rotate(axis, speed * Time.deltaTime * 60);
+	}
+}
